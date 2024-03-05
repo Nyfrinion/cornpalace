c8088686b4abbd034e49ae53e1893e144eefffb0
Nifrinion <andrewocornish@gmail.com>
2024-03-04 22:00:03 -0800

    v2.94

---
+++ dbconnection.session.sql
@@ -0,0 +1,52 @@
+DROP DATABASE shopdb;
+CREATE DATABASE shopdb;
+
+CREATE TABLE `shopdb`.`category` (
+    `category_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255)
+);
+
+CREATE TABLE `shopdb`.`products` (
+    `product_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `category_id` INT,
+    `name` VARCHAR(255),
+    `price` DECIMAL(10, 2),
+    FOREIGN KEY (`category_id`) REFERENCES `shopdb`.`category`(`category_id`)
+);
+
+CREATE TABLE `shopdb`.`orders` (
+    `order_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `customer_fname` VARCHAR(255),
+    `customer_lname` VARCHAR(255)
+);
+
+CREATE TABLE `shopdb`.`order_details` (
+    `order_detail_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `order_id` INT,
+    `product_id` INT,
+    `quantity` INT DEFAULT 1,
+    `color` VARCHAR(255),
+    `size` ENUM('XS', 'S', 'M', 'L', 'XL'),
+    `qr1` VARCHAR(255),
+    `qr2` VARCHAR(255),
+    `qr3` VARCHAR(255),
+    `qr4` VARCHAR(255),
+    `qr5` VARCHAR(255),
+    `qr6` VARCHAR(255),
+    `qr7` VARCHAR(255),
+    `qr8` VARCHAR(255),
+    `qr9` VARCHAR(255),
+    `qr10` VARCHAR(255),
+    `qr11` VARCHAR(255),
+    `qr12` VARCHAR(255),
+    `qr13` VARCHAR(255),
+    `qr14` VARCHAR(255),
+    `qr15` VARCHAR(255),
+    `qr16` VARCHAR(255),
+    `qr17` VARCHAR(255),
+    `qr18` VARCHAR(255),
+    `qr19` VARCHAR(255),
+    `qr20` VARCHAR(255),
+    FOREIGN KEY (`order_id`) REFERENCES `shopdb`.`orders`(`order_id`),
+    FOREIGN KEY (`product_id`) REFERENCES `shopdb`.`products`(`product_id`)
+);
